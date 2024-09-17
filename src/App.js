998cda2c42e9d495b32907a955bfdc50693cbae3
hamidr-source <hr454837@gmail.com>
2024-09-17 11:52:03 +0330

    Add color variable for css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import ThemeChanger from "./components/ThemeChanger/ThemeChanger";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <ThemeChanger />
+    </>
   );
 }
 
---
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const ThemeChanger = () => {
+  function handleClickBgColor() {}
+
+  return (
+    <button onClick={handleClickBgColor}>
+      <i className="fa-solid fa-sun"></i>
+    </button>
+  );
+};
+
+export default ThemeChanger;
