f2332ec53a80c8e64b21a1e80ea56291b2c35c4f
hamidr-source <hr454837@gmail.com>
2024-09-17 13:18:52 +0330

    Add dark style and some code for toggle

--- src/components/ThemeChanger/ThemeChanger.jsx
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
+const btn = document.querySelector("button");
+const icon = document.querySelector("i");
+
 const ThemeChanger = () => {
-  function handleClickBgColor() {}
+  const [toggle, setToggle] = useState(false);
+
+  function hansleChangeBgColor (e) {
+    setToggle(() => !toggle)
+    console.log(toggle)
+  }
 
   return (
-    <div className="theme">
-      <button onClick={handleClickBgColor}>
-        <i className="fa-solid fa-sun"></i>
+    <div>
+      <button className="theme" onClick={(e) => hansleChangeBgColor(e)}>
+        <i className="fa-solid fa-moon"></i>
       </button>
     </div>
   );
