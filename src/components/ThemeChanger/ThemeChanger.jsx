2d7f6fb82732c4b612a0549fe6b089a23d6c1b1e
hamidr-source <hr454837@gmail.com>
2024-09-17 19:21:43 +0330

    Add theme change for body

--- src/components/ThemeChanger/ThemeChanger.jsx
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -3,11 +3,22 @@ import "./style.css";
 
 const ThemeChanger = () => {
   const [toggle, setToggle] = useState(false);
-  
+
+  if (toggle) {
+    document.body.style.color = "#fff";
+    document.body.style.backgroundColor = "#2e2e47b0";
+  } else {
+    document.body.style.color = "#2e2e47b0";
+    document.body.style.backgroundColor = "#fff";
+  }
+
   return (
     <div>
-      <button className={toggle ? "theme dark" : "theme" } onClick={() => setToggle(!toggle)}>
-        <i className={toggle ? "fa-solid fa-moon" : "fa-solid fa-sun"}></i>
+      <button
+        className={toggle ? "theme dark" : "theme"}
+        onClick={() => setToggle(!toggle)}
+      >
+        <i className={toggle ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i>
       </button>
     </div>
   );
