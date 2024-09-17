a7edb7ce127d10fa86580f0daac13cc5931e9e1e
hamidr-source <hr454837@gmail.com>
2024-09-17 13:32:36 +0330

    Refactor the code

--- src/components/ThemeChanger/ThemeChanger.jsx
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
 import "./style.css";
 
-const btn = document.querySelector("button");
-const icon = document.querySelector("i");
-
 const ThemeChanger = () => {
   const [toggle, setToggle] = useState(false);
-
-  function hansleChangeBgColor (e) {
-    setToggle(() => !toggle)
-  }
-
+  
   return (
     <div>
-      <button className={toggle ? "theme dark" : "theme" } onClick={(e) => hansleChangeBgColor(e)}>
+      <button className={toggle ? "theme dark" : "theme" } onClick={() => setToggle(!toggle)}>
         <i className={toggle ? "fa-solid fa-moon" : "fa-solid fa-sun"}></i>
       </button>
     </div>
